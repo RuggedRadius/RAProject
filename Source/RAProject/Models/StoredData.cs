4820c9b3855414956f298d07819a8ef30580ce3c
Ben Royans <ben.royans@gmail.com>
2020-05-16 15:35:54 +0800

    Some UI and class migration

---
+++ Source/RAProject/Models/StoredData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAProject.Models
+{
+    [Serializable]
+    public class StoredData
+    {
+        public List<SupportedConsole> consoles;
+        public StoredData()
+        {
+            consoles = new List<SupportedConsole>();
+        }
+    }
+}
---
+++ Source/RAProject/Utilities/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAProject.Connection
+{
+    class Utilities
+    {
+        public static bool InternetConnected ()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    Console.WriteLine("Internet connection status: CONNECTED");
+                    return true;
+            }
+            catch
+            {
+                Console.WriteLine("Internet connection status: DISCONNECTED");
+                return false;
+            }
+        }
+    }
+}
